bc9bcbce6ec008addfafad4ac1a050338282eaf5
misobae <misobae0731@gmail.com>
2025-01-10 13:25:55 +0900

    chore: react-query-devtools 설치

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./tailwind.css";
 import Router from "./routes/index.tsx";
 import { Toaster } from "./components/ui/toaster.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthProvider } from "./context/auth-context.tsx";
 
 const queryClient = new QueryClient();
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
         <Router />
         <Toaster />
       </AuthProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
